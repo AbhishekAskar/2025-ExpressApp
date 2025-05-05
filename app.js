a6d3ada47a9ac929d681c0f4208318bcf153f141
Abhishek <abhiaskar.18@gmail.com>
2025-05-05 23:35:11 +0530

    Custom Middleware

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 const express = require('express');
-
 const app = express();
-
-app.use((req, res, next) => {
-    console.log("Output from Middleware 1");
-    next();
-})
-
-app.use((req, res, next) => {
-    console.log("Output from Middleware 2");
-    res.send("<h1>Hello Abhishek</h1>")
-})
-
-
 const port = 3000;
-app.listen(port, ()=>{
-    console.log("Server is up and running on port 3000! Ready to handle requests.")
+
+const customMiddleware = (req, res, next) => {
+    req.user = "Guest";
+    next();
+}
+
+app.get('/welcome', customMiddleware, (req,res) => {
+    res.send(`<h1>Welcome, ${req.user}</h1>`)
+})
+
+app.listen(port, () => {
+    console.log("Server is running on port 3000");
 });
