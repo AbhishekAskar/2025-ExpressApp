88eaa5f920b6fe38b357f22fd35bde9efe028242
Abhishek <abhiaskar.18@gmail.com>
2025-05-06 15:13:28 +0530

    Router and Filters

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 4000;
+const orderRouter = require('./routes/orders')
+const userRouter = require('./routes/users')
 
-app.use((req, res, next) => {
-    console.log(`${req.method} request made to ${req.url}`);
-    next(); 
-});
+app.use("/orders", orderRouter);
+app.use("/users", userRouter);
 
-app.get('/products', (req, res)=>{
-    res.send("Here is the list of all products.");
-})
-
-app.post('/products', (req, res)=>{
-    res.send("A new product has been added");
-})
-
-app.get('/categories', (req, res)=>{
-    res.send("Here is the list of all categories.");
-})
-
-app.post('/categories', (req, res)=>{
-    res.send("A new category has been created");
-})
-
-app.listen(port, () => {
+app.listen(4000, () => {
     console.log("Server is running on port 3000");
 });
