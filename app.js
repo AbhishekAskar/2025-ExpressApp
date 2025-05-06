8c0573a03d128f9d3f06e10aa3466d171275f19b
Abhishek <abhiaskar.18@gmail.com>
2025-05-06 18:19:21 +0530

    Crud implementation in Express

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const booksRouter = require('./routes/books')
+const studentRoutes = require("./routes/students");
+const courseRoutes = require("./routes/course");
 
-app.use("/books", booksRouter);
+const PORT = 3000;
 
-app.listen(4000, () => {
-    console.log("Server is running on port 3000");
+app.get("/", (req, res) => {
+  res.send("Welcome to the Student & Course Portal API!");
+});
+
+app.use("/students", studentRoutes);
+app.use("/courses", courseRoutes);
+
+app.use((req, res) => {
+  res.status(404).send("Page not found");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
