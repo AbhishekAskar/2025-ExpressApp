b84498fde66d17333565aa05b43d6bb3f5107a58
Abhishek <abhiaskar.18@gmail.com>
2025-05-06 14:32:47 +0530

    Server with Middleware for logging HTTP Method and endpoint

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 4000;
 
-app.get('/welcome/:username', (req, res)=>{
-    const name = req.params.username;
-    const role = req.query.role;
-    res.send(`Hello my name is ${name}, and my role is ${role}.`)
+app.use((req, res, next) => {
+    console.log(`${req.method} request made to ${req.url}`);
+    next(); 
+});
+
+app.get('/products', (req, res)=>{
+    res.send("Here is the list of all products.");
+})
+
+app.post('/products', (req, res)=>{
+    res.send("A new product has been added");
+})
+
+app.get('/categories', (req, res)=>{
+    res.send("Here is the list of all categories.");
+})
+
+app.post('/categories', (req, res)=>{
+    res.send("A new category has been created");
 })
 
 app.listen(port, () => {
