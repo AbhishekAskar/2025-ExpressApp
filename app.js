fd65c1569f4f48edf2243004502e0d57d530e880
Abhishek <abhiaskar.18@gmail.com>
2025-05-06 14:09:00 +0530

    Dynamic Routes, Query params and Route params

--- app.js
+++ app.js
@@ -2,24 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/products', (req, res) =>{
-    res.send("<h1>Here is the list of all products</h1>");
-})
-
-app.post('/products', (req, res, next) => {
-    res.send("A new order has been added");
-})
-
-app.get('/categories', (req, res) =>{
-    res.send("<h1>Here is the list of all categories</h1>");
-})
-
-app.post('/categories', (req, res, next) => {
-    res.send("A new category has been created");
-})
-
-app.use((req, res) =>{
-    res.status(404).send('<h1>404 - Page Not Found</h1>');
+app.get('/welcome/:username', (req, res)=>{
+    const name = req.params.username;
+    const role = req.query.role;
+    res.send(`Hello my name is ${name}, and my role is ${role}.`)
 })
 
 app.listen(port, () => {
